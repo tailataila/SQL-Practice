4fa13e149bc8d66bbf218d3a1cd210d7a329f08e
tailataila <73496700+tailataila@users.noreply.github.com>
2024-05-05 15:56:06 -0400

    inserting data & update & delete

--- sql-tutorial.sql
+++ sql-tutorial.sql
@@ -2,13 +2,24 @@
 CREATE TABLE student (
   student_id INT PRIMARY KEY,
   name VARCHAR(20),
-  majosr VARCHAR(20)
+  major VARCHAR(20)
 );
--- alternative <PRIMARY KEY (student_id)>
+
+-- alternative <PRIMARY KEY(student_id)>, AUTO_INCREMENT - auto numeration, NOT NULL - should be, UNIQUE - unique
+CREATE TABLE student (
+  student_id INT AUTO_INCREMENT,
+  name VARCHAR(20) NOT NULL,
+  major VARCHAR(20) UNIQUE,
+  PRIMARY KEY(student_id)
+);
+
 
 -- Show the table
 DESCRIBE srudent;
 SELECT * FROM student;
+
+-- Execute current statement
+-- CTRL + ENTER
 
 -- Delete table
 DROP TABLE student;
@@ -24,3 +35,39 @@ ALTER TABLE student DROP COLUMN gpa;
 -- BLOB - images, files, Binary Large Object, stores large data
 -- DATE - 'YYYY-MM-DD'
 -- TIMESTAMP - 'YYY-MM-DD HH:MM:SS', used for recording
+
+-- INSERT DATA
+INSERT INTO student VALUES(1, 'Jack', 'Biology');
+INSERT INTO student(student_id, name VALUE(3, 'Kale');
+
+-- UPDATE & DELETE
+UPDATE student
+SET major = 'Bio'
+WHERE major = 'Biology';
+
+UPDATE student
+SET major = 'Bio'
+WHERE student_id = 4;
+
+UPDATE student
+SET major = 'Bio'
+WHERE major = 'Biology' OR major = 'Chemistry';
+
+-- whithout WHERE is will apply to every row
+UPDATE srudent
+SET name = 'Tom', major = 'undecided'
+WHERE student_id = 1;
+
+-- delete all
+DELETE FROM student;
+
+-- condition
+DELETE FROM student
+WHERE student_id = 5;
+
+-- =    : equal
+-- <>   : not equal
+-- >    : greater than
+-- <    : less than
+-- >=   : greater than or equal
+-- <=   : less than or equal
