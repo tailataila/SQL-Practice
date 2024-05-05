d968f31711baef2afae7465caef9d2d632ce6c13
tailataila <73496700+tailataila@users.noreply.github.com>
2024-05-04 22:55:58 -0400

    extra space removed

--- tms-scripts.sql
+++ tms-scripts.sql
@@ -3,8 +3,6 @@
 -- 3. 2 + display all actions per task (SD_LIVE.TMS_ACTION_V, SD_LIVE.TMS_ACTION_FULL_TEXT_V)
 -- 4. Find all tasks that ever was assigned to PETR S at certain moment
 
-
-
 SELECT *
 FROM SD_LIVE.TMS_PROBLEM_V
 WHERE owner = 'PETR S' AND status12 NOT LIKE 'C%';
@@ -17,8 +15,6 @@ FROM SD_LIVE.TMS_ACTION_V;
 
 SELECT *
 FROM SD_LIVE.TMS_ACTION_FULL_TEXT_V;
-
-
 
                --aa_id
                --task_aa_id
@@ -33,15 +29,10 @@ FROM SD_LIVE.TMS_ACTION_FULL_TEXT_V;
                -- action_aa_id
                -- actionfulltext
 
-
-
-
 -- Task 1 Find all OPEN tasks owned by 'PETR S'. Show task id,  and status
 SELECT Concat(Concat(client, '-'), id) AS task_id, status12
 FROM SD_LIVE.TMS_PROBLEM_V                
 WHERE owner = 'PETR S' AND status12 NOT LIKE 'C%';
-
- 
 
 -- Task 2
 SELECT Concat(Concat(client, '-'), id) AS task_id, status12, SD_LIVE.TMS_PROBLEM_FULL_TEXT.problemfulltext
@@ -49,8 +40,6 @@ FROM SD_LIVE.TMS_PROBLEM_V
 JOIN SD_LIVE.TMS_PROBLEM_FULL_TEXT
 ON SD_LIVE.TMS_PROBLEM_V.aa_id = SD_LIVE.TMS_PROBLEM_FULL_TEXT.task_aa_id
 WHERE owner = 'PETR S' AND status12 NOT LIKE 'C%';
-
-
 
 -- Task 3
 SELECT Concat(Concat(client, '-'), id) AS task_id, status12, SD_LIVE.TMS_PROBLEM_FULL_TEXT.problemfulltext, SD_LIVE.TMS_ACTION_FULL_TEXT_V.actionfulltext
@@ -62,8 +51,6 @@ ON SD_LIVE.TMS_PROBLEM_V.aa_id = SD_LIVE.TMS_ACTION_V.pr_ac
 JOIN SD_LIVE.TMS_ACTION_FULL_TEXT_V
 ON SD_LIVE.TMS_ACTION_V.aa_id = SD_LIVE.TMS_ACTION_FULL_TEXT_V.action_aa_id
 WHERE owner = 'PETR S' AND status12 NOT LIKE 'C%';
-
-
 
 -- Task 4
 SELECT Concat(Concat(client, '-'), id) AS task_id, SD_LIVE.TMS_ACTION_V.assignedto
